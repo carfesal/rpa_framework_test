a87cc642820e6c78a1c4a4b7d35e0ebf6ed364e6
Carlos Salazar Moreira <56647144+carfesal@users.noreply.github.com>
2023-04-20 15:57:33 -0500

    feature: filling the search input and making the search

--- task.py
+++ task.py
@@ -1,9 +1,25 @@
 """Template robot with Python."""
+from RPA.Browser.Selenium import Selenium
+import time
+browser = Selenium(auto_close=False)
 
 
 def minimal_task():
-    print("Done.")
+    browser.open_available_browser("https://www.nytimes.com/")
+    time.sleep(5)
+    #click on the search button
+    browser.find_element("//button[@data-test-id='search-button']").click()
+    
+    #enter the search phrase
+    browser.find_element("//input[@data-testid='search-input']").send_keys("coronavirus")
 
+    #do the search
+    browser.find_element("//button[@data-test-id='search-submit']").click()
+    time.sleep(5)
+
+    #search the table of articles
+    
+# data-testid="search-input"
 
 if __name__ == "__main__":
     minimal_task()
